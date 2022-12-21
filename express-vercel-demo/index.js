f095965964c213911d2ed0bd247adfeb3e26bf6f
Fabricio84 <fabricio.abner@gmail.com>
2022-12-20 22:52:50 -0300

    outro projeto teste deploy vercel

---
+++ express-vercel-demo/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.use("/", (req, res) => {
+    res.json({ message: "Hello From Express App"});
+});
+
+app.listen(9000, () => {
+    console.log(`Starting server in port ${port}`);
+});
